650c7bf792bba3fae28d2736638f8713e4f2e5dc
yohansngus <yohansngus8@gmail.com>
2026-01-14 03:23:05 -0800

    Day 1 - React setup and counter app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const decrement = () => {
+    setCounter(counter - 1);
+  };
+  const increment = () => {
+    setCounter(counter + 1);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="reset">Reset</div>
+        <div className="counter">{counter}</div>
+        <div className="controller">
+          <div className="decrement" onClick={() => decrement()}>
+            -
+          </div>
+          <div className="increment" onClick={() => increment()}>
+            +
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
